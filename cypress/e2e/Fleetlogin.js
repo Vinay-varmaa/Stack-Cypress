9560c7271d56c2ee011be921c658d97e22b59258
geddamsaivamsi <saivamsi.geddam@fleetenable.com>
2025-03-18 16:54:13 +0530

    chnages

--- cypress/e2e/Fleetlogin.js
+++ cypress/e2e/Fleetlogin.js
@@ -1,18 +1,11 @@
-/// <reference types="cypress" />
-
-describe('Login page', () => {
-  beforeEach('The login page consists of',()=>{
-    cy.visit("https://stage.stackenable.com")
-  })
-  it('Stack login page',()=>{
-    cy.get("[alt = 'Stack Enable Logo']")
-    cy.get("[class='loginInputs flex justify-content-center']").find("input").eq(0).type("wms.logisticsstudio@gmail.com")
-    cy.get("[class='loginInputs flex justify-content-center']").find("input").eq(1).type("Test@1234").then(()=>{
-      cy.log('Test failed : "Incorrect Password"')
-    })
-    cy.get(".p-button-label").click();
-    cy.pathname('/home').then(()=>{
-      cy.log('Test Passed : "Logged in Successfully"')
-    })
+Cypress.on ('uncaught:exception', (err, runnable) => {
+  return false;
+});
+describe('Login validation', () => {
+  it('should successfully log in', () => {
+    cy.visit('https://fe-qa.fleetenable.com/login')
+    cy.get("input[placeholder='Enter Email or Mobile Number']").type('fe-qa@fleetenable.com')
+    cy.get("input[type='password']").type('test1234')
+    cy.get('button[type="submit"]').click()
   })
 })
