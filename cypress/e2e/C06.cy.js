45eed7590ea39cf98da31c794b508350b3e9cea3
Varma <vinayvarma.nandhyala@imaginnovate.com>
2025-04-22 22:16:41 +0530

    New Scripts from Tuskr

--- cypress/e2e/C06.cy.js
+++ cypress/e2e/C06.cy.js
@@ -13,18 +13,24 @@ describe('Validation of Shipper and Product Details in the Create Order', () => 
         cy.fixture('shipperAndProducts').then((data) => {
             cy.get("[class='sidebar-full']").find("app-side-nav-option").find("a[href='/products']").click();
             cy.get("[class='inputField formFields']").click();
+            cy.wait(1000)
             cy.get("[class='p-dropdown-filter-container ng-star-inserted']").type(data.shipperName).wait(2000);
             cy.get("[role='listbox']").find("li").eq(0).click().wait(2000);
 
             let productNames = [];
 
             cy.get("[class='p-element p-datatable-tbody']").find("tr").each(($row) => {
-                cy.wrap($row).find("td").eq(1).invoke("text").then((productName) => {
-                    productNames.push(productName.trim());
-                });
+                cy.wrap($row).find("td").eq(1).then(($ExactName)=>{
+                    cy.wrap($ExactName).find("span").eq(3).invoke("text").then((productName) => {
+                        productNames.push(productName.trim());
+                        cy.log(productNames)
+                        cy.log("Collected Products:", JSON.stringify(productNames));
+                    });
+                })
             }).then(() => {
-                cy.log(productNames);
-                expect(productNames).to.deep.eq(data.productsList, 'Products matched');
+            //     cy.log(productNames);
+                const sortedProducts = data.producstList.sort();
+                expect(JSON.stringify(productNames.sort())).to.deep.equal(JSON.stringify(sortedProducts), 'Products matched');
             });
         });
     });
